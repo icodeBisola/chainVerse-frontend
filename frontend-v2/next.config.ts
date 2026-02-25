e87c99181ab5ea170ae902b60bcff91eda06f867
mogbonjubolaolasunkanmi-art <mogbonjubolaolasunkanmi@gmail.com>
2026-02-25 10:05:40 +0100

    commit solution for advance setup and refactoring

--- frontend-v2/next.config.ts
+++ frontend-v2/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
+import type  withBundleAnalyzer  from "@next/bundle-analyzer";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const withBundleAnalyzer:any= {
+  enabled: process.env.ANALYZE === "true",
 };
 
-export default nextConfig;
+const nextConfig:NextConfig = {
+  reactStrictMode: true,
+};
+
+export default  withBundleAnalyzer(nextConfig);
